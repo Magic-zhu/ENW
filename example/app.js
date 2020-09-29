ea14699fe7556839d64d9e577ca79b74100de294
magic-zhu <18817313320@163.com>
2020-09-29 18:53:03 +0800

    update

---
+++ example/app.js
@@ -0,0 +1,6 @@
+const Enw  = require ('../libs/application')
+const app = new Enw();
+
+app.listen(3001,()=>{
+    console.log("服务器启动了 3001")
+})
--- libs/application.js
+++ libs/application.js
@@ -1,17 +1,43 @@
-const http = require('http')
-module.exports = class Application extends Emmiter{
+'use strict'
+
+const http = require('http');
+const Emitter = require('events');
+const request = require("./request");
+const response = require('./response');
+
+module.exports = class Application extends Emitter{
 
     constructor(options){
         super();
         options = options||{};
+        this.server = null;
+        this.middleware = {};
+        this.request = Object.create(request);
+        this.response = Object.create(response);
     }
     
-    createHttpServer(){
-
-    }
-
+    /**
+     * 端口监听
+     * @param  {...any} args
+     * @api public 
+     */
     listen(...args){
-        const server = http.createServer();
+        const server = http.createServer(this.handleRequest);
+        this.server = server;
         return server.listen(...args);
     }
+
+    /**
+     * 处理请求
+     * @param {*} req 
+     * @param {*} res 
+     * @api private
+     */
+    handleRequest(req,res){
+        console.log(req)
+        res.writeHead(200, {'Content-type' : 'text/html'});
+        res.write('<h1>Node.js</h1>');
+        res.end('<p>Hello World</p>');
+    }
+
 }
